352e76bfbdfc0c26e65c6be9beb6d7f1cf1832c3
ryanipam <ryanipam@gmail.com>
2022-05-21 08:28:53 +0700

    beli telur

--- main.py
+++ main.py
@@ -2,4 +2,15 @@
 ini adalah file percobaan pertama
 """
 
-print ("hello world")
+susu = input("Apakah susu ada?")
+beli = 'tidak ada yg dibeli'
+if susu == 'y':
+    telur = input("Apakah ada Telur?")
+    if telur == 'y':
+        beli = 'telur 6 dan susu 1'
+    else:
+        beli = 'susu 1'
+
+    print(beli)
+else:
+    print(beli)
